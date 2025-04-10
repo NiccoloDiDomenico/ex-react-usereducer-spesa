fc6da0e501ed7f36fe010fa4a5bfd71236e13285
NiccoloDiDomenico <niccolodidomenico99@gmail.com>
2025-04-10 18:08:21 +0200

    complete milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { useState } from 'react'
 
 function App() {
+  const [addedProducts, setAddedProducts] = useState([])
+  console.log(addedProducts);
+
+
   const products = [
     { name: 'Mela', price: 0.5 },
     { name: 'Pane', price: 1.2 },
@@ -8,19 +12,51 @@ function App() {
     { name: 'Pasta', price: 0.7 },
   ];
 
+  const addToCart = (product) => {
+    const isProductAdded = addedProducts.some((p) => p.name === product.name);
+    if (!isProductAdded) {
+      setAddedProducts((curr) => [...curr, {
+        ...product,
+        quantity: 1
+      }]);
+    } else {
+      return;
+    };
+  };
 
   return (
     <>
       <h3>Lista della spesa</h3>
       <ol>
-        {products.map((p) => (
-          <li>
+        {products.map((p, i) => (
+          <li key={i}>
             <span>{p.name}, {p.price.toFixed(2)}€</span>
+            <button onClick={() => addToCart(p)}>Aggiungi al carrello</button>
           </li>
         ))}
       </ol>
+      {
+        addedProducts.length > 0 ? (
+          <>
+            <h3>Carrello</h3>
+            <ol>
+              {addedProducts.map((p, i) => (
+                <li key={i}>
+                  <span>Articolo: {p.name}, Prezzo: {p.price.toFixed(2)}€, Quantità: {p.quantity}</span>
+                </li>
+              ))}
+            </ol>
+          </>
+        ) : (
+          <>
+            <h3>Carrello</h3>
+            <p>Non ci sono elementi...</p>
+          </>
+        )
+      }
     </>
   )
 }
 
 export default App
+

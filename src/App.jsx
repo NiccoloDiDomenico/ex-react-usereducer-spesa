89b87a9d11a0f88a5b56de6658e6ac90dceeffc8
NiccoloDiDomenico <niccolodidomenico99@gmail.com>
2025-04-10 17:30:25 +0200

    complete milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import { useState } from 'react'
 
 function App() {
+  const products = [
+    { name: 'Mela', price: 0.5 },
+    { name: 'Pane', price: 1.2 },
+    { name: 'Latte', price: 1.0 },
+    { name: 'Pasta', price: 0.7 },
+  ];
+
 
   return (
     <>
-
+      <h3>Lista della spesa</h3>
+      <ol>
+        {products.map((p) => (
+          <li>
+            <span>{p.name}, {p.price.toFixed(2)}€</span>
+          </li>
+        ))}
+      </ol>
     </>
   )
 }
